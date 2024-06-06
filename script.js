93d3b37664f71b2f669151ad74cf476caced7c81
Soumyaranjan Singh <158814057+Soumya7682@users.noreply.github.com>
2024-06-06 13:53:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let display = document.getElementById('inputCAl');
+let buttons = document.querySelectorAll('button');
+let buttonsArray = Array.from(buttons);
+let str = '';
+buttonsArray.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        if (e.target.innerHTML == 'DEL') {
+            str = str.substring(0, str.length - 1);
+            display.value = str;
+        }
+        else if (e.target.innerHTML == 'AC'
+        ) {
+            str = '';
+            display.value = str;
+
+
+        } else if (e.target.innerHTML == '='
+        ) {
+            str = eval(str);
+            display.value = str;
+
+        }
+        else {
+            str += e.target.innerHTML;
+            display.value = str;
+        }
+
+    })
+});
+
+
